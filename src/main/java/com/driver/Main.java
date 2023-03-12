4c7b8db4202adc891d3a28c43adbfb3c9f115d69
shubham <shubham.hatkar9765@gmail.com>
2023-03-12 19:19:09 +0530

    third change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,14 +3,14 @@ package com.driver;
 
 public class Main
 {
-    class A
+    static class A
     {
         String meth()
         {
             return "Invoking method from class A";
         }
     }
-    class B extends A
+    static class B extends A
     {
         String meth()
         {
